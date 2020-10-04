37a83b923a4540b798699e5accd6c87622284aec
Walid Amriou <walidamriou@gmail.com>
2020-10-04 14:11:17 +0100

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+/*
+ ********************************************************************
+  ESP32_PWM_Generator_Arduino
+  Implement PWM generator for Espressif Systems ESP32 boards by Arduino framework  
+
+  Github: https://github.com/walidamriou/ESP32_PWM_Generator_Arduino
+  Copyright © 2020 Walid Amriou
+  Last update: October 2020
+ ********************************************************************
+*/
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+uint8_t LEDpin = 5; //5
+
+/* By default PWM frequency is 1000Hz and we are using same 
+   for this application hence no need to set */
+
+void setup(){
+  Serial.begin(115200);
+  analogWriteFreq(1);
+  //analogWriteRange(255);
+}
+
+void loop(){
+  analogWrite(LEDpin, 512);  /* set initial 50% duty cycle */
+
+}
